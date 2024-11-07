f27890f10eb626174d171a3afcc589d55be76689
Mohammed Bouali <Med.bouali96@hotmail.com>
2024-11-07 20:33:29 +0100

    updated-websocket-url-1

--- notification-service/src/main/java/com/individual_s7/notification_service/configuration/WebSocketConfig.java
+++ notification-service/src/main/java/com/individual_s7/notification_service/configuration/WebSocketConfig.java
@@ -82,7 +82,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://react-app:5173")
+                .setAllowedOriginPatterns("*")
                 .addInterceptors(jwtHandshakeInterceptor);
     }
 
